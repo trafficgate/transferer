ac2d45f4466c06828fe79066ac801e4805efe94a
Andrew Miller <andrew.miller@rakuten.com>
2019-05-17 18:20:54 +0900

    Set empty quotes based on symfony/process version

---
+++ tests/CommandTestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Trafficgate\Transferer;
+
+use PackageVersions\Versions;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class CommandTestCase extends TestCase
+{
+    /**
+     * Symfony/process returns either '' or "" depending on version.
+     *
+     * Versions less than or equal to 3.4.X return ''.
+     * Versions greather than or equal to 4.0.0 return "".
+     *
+     * @var string
+     */
+    public $emptyQuotes;
+
+    public function setUp()
+    {
+        $this->emptyQuotes = "''";
+
+        $symfonyProcessVersion = Versions::getVersion('symfony/process');
+
+        if (preg_match('/^v4/', $symfonyProcessVersion)) {
+            $this->emptyQuotes = '""';
+        }
+    }
+}
--- tests/Transfer/ScpTransferTest.php
+++ tests/Transfer/ScpTransferTest.php
@@ -2,10 +2,11 @@
 
 namespace Trafficgate\Transferer\Transfer;
 
-use PHPUnit\Framework\TestCase;
+use PackageVersions\Versions;
+use Trafficgate\Transferer\CommandTestCase;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 
-class ScpTransferTest extends TestCase
+class ScpTransferTest extends CommandTestCase
 {
     public function testConstruct()
     {
@@ -18,17 +19,17 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->protocol1();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-1' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-1' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $return = $scpTransfer->protocol2();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-2' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-2' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->protocol2($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->protocol1()->protocol1($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testIp4OnlyAndIp6Only()
@@ -36,17 +37,17 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->ip4Only();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-4' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-4' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $return = $scpTransfer->ip6Only();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-6' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-6' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->ip6Only($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->ip4Only()->ip4Only($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testBatchMode()
@@ -54,9 +55,9 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->batchMode();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-B' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-B' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
         $scpTransfer->batchMode($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testCompression()
@@ -64,9 +65,9 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->compression();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-C' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-C' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
         $scpTransfer->compression($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     /**
@@ -84,13 +85,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->cipher('blowfish-cbc');
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-c' 'blowfish-cbc' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-c' 'blowfish-cbc' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->cipher('aes256-cbc');
-        $this->assertEquals("'scp' '-c' 'aes256-cbc' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-c' 'aes256-cbc' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->cipher($value = '', $remove = false, $enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testSshConfig()
@@ -98,13 +99,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->sshConfig('/some/fake/path/to/config');
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-F' '/some/fake/path/to/config' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-F' '/some/fake/path/to/config' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->sshConfig('/another/fake/path/to/config');
-        $this->assertEquals("'scp' '-F' '/another/fake/path/to/config' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-F' '/another/fake/path/to/config' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->sshConfig($value = '', $remove = false, $enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testIdentityFile()
@@ -112,13 +113,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->identityFile('/some/fake/path/to/id_rsa');
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-i' '/some/fake/path/to/id_rsa' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-i' '/some/fake/path/to/id_rsa' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->identityFile('/another/fake/path/to/id_rsa');
-        $this->assertEquals("'scp' '-i' '/another/fake/path/to/id_rsa' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-i' '/another/fake/path/to/id_rsa' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->identityFile($value = '', $remove = false, $enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testLimit()
@@ -126,13 +127,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->limit(200);
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-l' '200' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-l' '200' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->limit(400);
-        $this->assertEquals("'scp' '-l' '400' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-l' '400' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->limit($value = '', $remove = false, $enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testSshOptions()
@@ -140,19 +141,19 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->sshOptions('Host distant');
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-o' 'Host distant' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-o' 'Host distant' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->sshOptions('ProxyCommand ssh near dc distant 22');
         $this->assertEquals(
-            "'scp' '-o' 'Host distant' '-o' 'ProxyCommand ssh near dc distant 22' \"\" \"\"",
+            "'scp' '-o' 'Host distant' '-o' 'ProxyCommand ssh near dc distant 22' {$this->emptyQuotes} {$this->emptyQuotes}",
             $scpTransfer->getCommandString()
         );
 
         $scpTransfer->sshOptions($value = 'Host distant', $remove = true);
-        $this->assertEquals("'scp' '-o' 'ProxyCommand ssh near dc distant 22' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-o' 'ProxyCommand ssh near dc distant 22' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->sshOptions($value = 'ProxyCommand ssh near dc distant 22', $remove = true);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testPort()
@@ -160,13 +161,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->port(54321);
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-P' '54321' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-P' '54321' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->port(12345);
-        $this->assertEquals("'scp' '-P' '12345' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-P' '12345' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->port($value = '', $remove = false, $enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testPreserveFile()
@@ -174,9 +175,9 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->preserveFile();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-p' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-p' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
         $scpTransfer->preserveFile($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testQuietMode()
@@ -184,9 +185,9 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->quietMode();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-q' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-q' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
         $scpTransfer->quietMode($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testRecursive()
@@ -194,9 +195,9 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->recursive();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-r' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-r' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
         $scpTransfer->recursive($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testProgram()
@@ -204,13 +205,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->program('some_program');
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-S' 'some_program' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-S' 'some_program' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->program('another_program');
-        $this->assertEquals("'scp' '-S' 'another_program' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-S' 'another_program' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->program($value = '', $remove = false, $enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testVerbose()
@@ -218,9 +219,9 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->verbose();
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '-v' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '-v' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
         $scpTransfer->verbose($enabled = false);
-        $this->assertEquals("'scp' \"\" \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testSource()
@@ -228,13 +229,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->source('/path/to/file');
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' '/path/to/file' \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' '/path/to/file' {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->source('/path/to/file', 'host');
-        $this->assertEquals("'scp' 'host:/path/to/file' \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' 'host:/path/to/file' {$this->emptyQuotes}", $scpTransfer->getCommandString());
 
         $scpTransfer->source('/path/to/file', 'host', 'user');
-        $this->assertEquals("'scp' 'user@host:/path/to/file' \"\"", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' 'user@host:/path/to/file' {$this->emptyQuotes}", $scpTransfer->getCommandString());
     }
 
     public function testDestination()
@@ -242,13 +243,13 @@ class ScpTransferTest extends TestCase
         $scpTransfer = new ScpTransfer();
         $return      = $scpTransfer->destination('/path/to/file');
         $this->assertSame($scpTransfer, $return);
-        $this->assertEquals("'scp' \"\" '/path/to/file'", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} '/path/to/file'", $scpTransfer->getCommandString());
 
         $scpTransfer->destination('/path/to/file', 'host');
-        $this->assertEquals("'scp' \"\" 'host:/path/to/file'", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} 'host:/path/to/file'", $scpTransfer->getCommandString());
 
         $scpTransfer->destination('/path/to/file', 'host', 'user');
-        $this->assertEquals("'scp' \"\" 'user@host:/path/to/file'", $scpTransfer->getCommandString());
+        $this->assertEquals("'scp' {$this->emptyQuotes} 'user@host:/path/to/file'", $scpTransfer->getCommandString());
     }
 
     public function testSourceAndDestination()
